d293b01643f065c9ded658852c16a3018947096d
Dean Exikanas <mexikanas@gmail.com>
2017-07-30 22:49:02 +1000

    Logged to check that PlayerController was correctly possessing tank on begin play

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,22 @@
 
 #include "Public/TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("The player possesses %s"), *ControlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("The Player controller has not possessed pawn"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
