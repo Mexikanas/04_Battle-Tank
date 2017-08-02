8c501f66f148acd2bf8ffbc1e8fe9da64a19adf9
Dean Exikanas <mexikanas@gmail.com>
2017-08-02 21:01:29 +1000

    Updated the Elevate function to actually move the barrel

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -61,5 +61,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
-	Barrel->Elevate(5.f);
+	Barrel->Elevate(DeltaRotator.Pitch);
 }
