d1e9eb93c714de0a726801bee46d7e4e90e72a91
Dean Exikanas <mexikanas@gmail.com>
2017-08-05 16:13:35 +1000

    Linked Left and Right Tracks to track movement through TankMovement Component

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,11 +2,24 @@
 
 #include "TankMovementComponent.h"
 #include "Engine/World.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(
+	UTankTrack* LeftTrackToSet, 
+	UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%f: %s throw: %f"), Time, *Name, Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
 
